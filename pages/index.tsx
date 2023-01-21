d27d0d533629485b76fc2ba718fca546943ce490
Leon <lshen@bol.com>
2023-01-21 19:00:30 +0100

    chore: Adds some based styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import { darkTheme, styled } from '../stitches.config';
+import { Text } from '../src/Components/Base/Text.styled';
+import { Box } from '../src/Components/Base/Box.styled';
 
 const Main = styled('main', {
 	display: 'flex',
@@ -8,8 +10,6 @@ const Main = styled('main', {
 	backgroundColor: '$background',
 	height: '100%',
 });
-
-const Text = styled('p', { color: '$text' });
 
 const Link = styled('a', {
 	color: '$text',
@@ -41,11 +41,33 @@ export default function Home() {
 				<title>Use Stitches with Next.js</title>
 			</Head>
 			<Text as="h1">Hello, from Stitches.</Text>
-			<Text as="h2">Hello Leon.</Text>
-			<Text>
-				For full documentation, visit{' '}
-				<Link href="https://stitches.dev">stitches.dev</Link>.
-			</Text>
+
+			<Box css={{ width: '50rem' }}>
+				<Text size={'xs'}>
+					This is some gibberish for testing the text styled component. I hope
+					that it will fill more than two lines.
+				</Text>
+
+				<Text size={'sm'}>
+					This is some gibberish for testing the text styled component. I hope
+					that it will fill more than two lines.
+				</Text>
+
+				<Text size={'md'}>
+					This is some gibberish for testing the text styled component. I hope
+					that it will fill more than two lines.
+				</Text>
+
+				<Text size={'lg'}>
+					This is some gibberish for testing the text styled component. I hope
+					that it will fill more than two lines.
+				</Text>
+
+				<Text size={'xl'}>
+					This is some gibberish for testing the text styled component. I hope
+					that it will fill more than two lines.
+				</Text>
+			</Box>
 
 			<Button onClick={toggleTheme}>Switch to {inActiveTheme}</Button>
 		</Main>
---
+++ src/Components/Base/Text.styled.tsx
@@ -0,0 +1,50 @@
+import { styled } from '../../../stitches.config';
+
+export const Text = styled('p', {
+	variants: {
+		weight: {
+			normal: {
+				fontWeight: '400',
+			},
+			bold: {
+				fontWeight: '600',
+			},
+		},
+		style: {
+			emphasis: {
+				fontStyle: 'italic',
+			},
+			normal: {
+				fontStyle: 'normal',
+			},
+		},
+		size: {
+			xs: {
+				fontSize: '1.2rem',
+				lineHeight: '1.8rem',
+			},
+			sm: {
+				fontSize: '1.4rem',
+				lineHeight: '2rem',
+			},
+			md: {
+				fontSize: '1.6rem',
+				lineHeight: '2.2rem',
+			},
+			lg: {
+				fontSize: '1.8rem',
+				lineHeight: '2.4rem',
+			},
+			xl: {
+				fontSize: '2rem',
+				lineHeight: '2.6rem',
+			},
+		},
+	},
+
+	defaultVariants: {
+		weight: 'normal',
+		style: 'normal',
+		size: 'md',
+	},
+});
